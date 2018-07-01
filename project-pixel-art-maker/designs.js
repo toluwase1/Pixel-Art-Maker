2f6d7918bfc71622829f2dcca367a3f732126ddb
toluwase1 <toluwasethomas1@gmail.com>
2018-07-01 17:17:25 +0100

    Pixel Art Maker - Udacity

--- project-pixel-art-maker/designs.js
+++ project-pixel-art-maker/designs.js
@@ -1,10 +1,38 @@
-// Select color input
-// Select size input
+//sets all the variables
+/* global document */
+let canvas = document.getElementById("pixel_canvas");
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+let sizePicker = document.getElementById("sizePicker");
+let color = document.getElementById("colorPicker");
 
-// When size is submitted by the user, call makeGrid()
+color.addEventListener("click", function(){});
+
+//An event listener that comes into action when Submit button is clicked
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+
 
 function makeGrid() {
+    for (let r=0; r<height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c=0; c<width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
 
-// Your code goes here!
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
 
+//A function which fills color in the cell that was clicked and changes the color.
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
 }
